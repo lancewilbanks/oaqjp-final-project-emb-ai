2fd1c46268cec7842248d680e6174ecf9ae724ab
lancewilbanks <lancemsacct@outlook.com>
2026-09-21 13:38:24 -0400

    Assessment 3a-3b completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -10,4 +11,23 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json = myobj, headers=headers)
 
-    return response.text
+    formatted_txt = json.loads(response.text)
+
+    emotion_dict = formatted_txt['emotionPredictions'][0]['emotion']    
+    
+    anger_score = emotion_dict['anger']
+    disgust_score = emotion_dict['disgust']  
+    fear_score = emotion_dict['fear']
+    joy_score = emotion_dict['joy']
+    sadness_score = emotion_dict['sadness']
+
+    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }    
